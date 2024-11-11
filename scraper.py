7f37f9684ec958eef80f4f8c6c113ca787d20125
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2024-11-11 06:18:11 -0600

    Scraping... will get some sleep and continue later today

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+from bs4 import BeautifulSoup
 
 # headers = {
 #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
@@ -15,4 +16,13 @@ import time
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 driver.get("https://quran411.com/")
-time.sleep(5)
+page = driver.page_source
+time.sleep(2)
+soup = BeautifulSoup(page, "html.parser")
+
+container = soup.find('div', class_ = "container")
+
+listItems = container.find_all("li")
+
+for i in listItems:
+    print(i.text)

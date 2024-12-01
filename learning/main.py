28b9c542ceb6a8115a30602df6606da6b9792471
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2024-12-01 03:30:53 -0600

    Getting started with tensors

--- learning/main.py
+++ learning/main.py
@@ -2,5 +2,28 @@ import torch
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
-torch.t 
+# scalar = torch.tensor([[2, 3, 4], [4, 5, 5]])
+# print(scalar.size)
+
+# new = np.load(r"C:\Users\dadaa\Muktashif\001001_mfccs.npy")
+# print(new.ndim)
+
+# Random_Tensor = torch.rand(3, 4)
+# print(Random_Tensor)
+
+# Image_Tensor = torch.rand(size=(224, 224, 3))
+# print(Image_Tensor)
+
+# zero_tensor = torch.zeros(3, 4)
+# print(zero_tensor)
+
+# one_tensor = torch.ones(3, 4)
+# print(one_tensor)
+
+# num = np.ones((2,3))
+# print(num)
+
+torch_range = torch.arange(0, 10)
+print(torch_range)

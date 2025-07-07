5bb4739101c20fcc463fb0dcdc0f3f96b83ef996
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2025-07-07 12:10:51 -0500

    testing

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import whisper
+import os
+import json
+os.environ["PATH"] += os.pathsep + r"C:\ffmpeg\bin"
+model = whisper.load_model("small")
+result = model.transcribe("001001.mp3", language="ar")
+with open("test.json", "w", encoding="utf-8") as file:
+    json.dump(result, file)
 

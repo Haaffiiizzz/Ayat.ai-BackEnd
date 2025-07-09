4ee2fa7ccfe0ba025561aba2f3d712b0fb705d6b
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2025-07-09 02:09:49 -0500

    Gotten a dataset to use + text matches. will need to test with different audios.

--- learning/main.py
+++ learning/main.py
@@ -1,11 +1,5 @@
-import torch
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import time
+import whisper
 
-x = torch.arange(1., 10.)
-print(x, x.shape)
-
-xReshaped = x.reshape(1, 7)
-print(xReshaped)
+model = whisper.load_model("small")
+result = model.transcribe("your_audio.mp3", language="ar")
+print(result["text"])
--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import json
 os.environ["PATH"] += os.pathsep + r"C:\ffmpeg\bin"
 model = whisper.load_model("small")
 result = model.transcribe("001001.mp3", language="ar")
-with open("test.json", "w", encoding="utf-8") as file:
-    json.dump(result, file)
+# with open("test.json", "w", encoding="utf-8") as file:
+#     json.dump(result, file)
+with open("test.txt", "r", encoding="utf-8") as file:
+    og = file.read().strip()
 
+print(result["text"].strip( ) == og)
+print(result["text"])
+print(og)

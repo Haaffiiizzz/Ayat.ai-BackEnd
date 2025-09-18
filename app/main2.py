d40a964711da9eb839600ad756bc141de77678a5
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2025-09-18 00:12:06 -0500

    new req

--- app/main2.py
+++ app/main2.py
@@ -2,11 +2,8 @@ import whisper
 import os
 import json
 from rapidfuzz import process, fuzz
-import time
 import tempfile
 import numpy as np
-import pandas as pd
-from sentence_transformers import SentenceTransformer
 from dotenv import load_dotenv
 load_dotenv()
 
